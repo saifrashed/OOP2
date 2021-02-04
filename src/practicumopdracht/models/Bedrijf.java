5ae6f3ba65c96cc5fe8afe60ab8359cfce9a9014
Saif <saifeddinerashed@icloud.com>
2021-02-04 15:57:27 +0100

    init

---
+++ src/practicumopdracht/models/Bedrijf.java
@@ -0,0 +1,10 @@
+package practicumopdracht.models;
+
+import java.util.ArrayList;
+
+public class Bedrijf {
+
+    private String naam;
+    private ArrayList<Persoon> personen = new ArrayList<>();
+
+}
---
+++ src/practicumopdracht/models/Persoon.java
@@ -0,0 +1,14 @@
+package practicumopdracht.models;
+
+import java.time.LocalDate;
+
+public class Persoon {
+
+    private String voornaam;
+    private String achternaam;
+    private LocalDate geboorteDatum;
+    private double lengte;
+    private int telefoonNummer;
+    private String email;
+
+}
